642e9c83aea968bf210d7a1ad4061e27c1b1dc30
Ben Balderas <benbalderas3@gmail.com>
2020-06-12 21:31:23 -0500

    created ducks & store

---
+++ starter-code/src/redux/CartDuck.js
@@ -0,0 +1,10 @@
+// Actions
+const REMOVE_FROM_CART = "ironstore/cart/REMOVE_FROM_CART";
+
+// Initial state
+
+// Reducer
+
+// Action Creator
+
+// Thunk
---
+++ starter-code/src/redux/ShopDuck.js
@@ -0,0 +1,12 @@
+// Actions
+const GET_PRODUCTS_SUCCESS = 'ironstore/store/GET_PRODUCTS_SUCCESS';
+const GET_PRODUCTS_ERROR = 'ironstore/store/GET_PRODUCTS_ERROR';
+const ADD_TO_CART = 'ironstore/store/ADD_TO_CART';
+
+// Initial state
+
+// Reducer
+
+// Action Creator
+
+// Thunk
