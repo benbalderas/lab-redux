b70bfbb36e2196a42a3907fbdfff9c63e4bd9d9a
Ben Balderas <benbalderas3@gmail.com>
2020-06-14 18:41:22 -0500

    create store actions, service and thunk

--- starter-code/src/redux/CartDuck.js
+++ starter-code/src/redux/CartDuck.js
@@ -1,9 +1,21 @@
 // Actions
+const ADD_TO_CART = "ironstore/cart/ADD_TO_CART";
 const REMOVE_FROM_CART = "ironstore/cart/REMOVE_FROM_CART";
 
-// Initial state
+// Initial stat
+const initialState = {
+  results: {},
+};
 
 // Reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case REMOVE_FROM_CART:
+      return "";
+    case ADD_TO_CART:
+      return "";
+  }
+}
 
 // Action Creator
 
--- starter-code/src/redux/ShopDuck.js
+++ starter-code/src/redux/ShopDuck.js
@@ -1,12 +1,55 @@
+import axios from "axios";
+
 // Actions
-const GET_PRODUCTS_SUCCESS = 'ironstore/store/GET_PRODUCTS_SUCCESS';
-const GET_PRODUCTS_ERROR = 'ironstore/store/GET_PRODUCTS_ERROR';
-const ADD_TO_CART = 'ironstore/store/ADD_TO_CART';
+const GET_PRODUCTS_SUCCESS = "ironstore/store/GET_PRODUCTS_SUCCESS";
+const GET_PRODUCTS_ERROR = "ironstore/store/GET_PRODUCTS_ERROR";
 
 // Initial state
+const initialState = {
+  loading: false,
+  results: {},
+  error: undefined,
+};
 
 // Reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        results: action.payload,
+      };
+    case GET_PRODUCTS_ERROR:
+      return {
+        ...state,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+}
 
 // Action Creator
+export const getProductsSuccess = (payload) => ({
+  type: GET_PRODUCTS_SUCCESS,
+  payload,
+});
+
+export const getProductsError = (error) => ({
+  type: GET_TODOS_ERROR,
+  error,
+});
 
 // Thunk
+export const getProducts = () => {
+  return (dispatch) => {
+    axios
+      .get("http://localhost:4000/products")
+      .then((res) => {
+        dispatch(getProductsSuccess(res.data));
+      })
+      .catch((res) => {
+        dispatch(getProductsError(res.response.data));
+      });
+  };
+};
